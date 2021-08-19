560290d1519a99a80554a9138c1f7ae473e168dd
SobirovI <88648901+SobirovI@users.noreply.github.com>
2021-08-19 15:20:35 +0500

    adding apk file

---
+++ build/google_map_location_picker/generated/source/buildConfig/release/com/humazed/google_map_location_picker/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.humazed.google_map_location_picker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.humazed.google_map_location_picker";
+  public static final String BUILD_TYPE = "release";
+}
